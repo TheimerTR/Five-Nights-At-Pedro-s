5c9a92d0f5d812f4079aa99f37e6e607b0a4dfbf
Icefenix7198 <eric.segovia@estudiantat.upc.edu>
2024-05-06 00:48:59 +0200

    freddy wip

---
+++ VR Project/Assets/Scripts/Freddy_Behaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Freddy_Behaviour : MonoBehaviour
+{
+    public List<Vector3> FreddyList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
