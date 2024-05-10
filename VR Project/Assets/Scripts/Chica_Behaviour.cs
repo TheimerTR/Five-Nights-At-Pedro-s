652c7d1b30e7faa1b3e64408972d90ac2ff65e61
TheimerTR <albertohidalgo268@gmail.com>
2024-05-10 12:27:26 +0200

    Chica Rework

--- VR Project/Assets/Scripts/Chica_Behaviour.cs
+++ VR Project/Assets/Scripts/Chica_Behaviour.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using static Unity.VisualScripting.Member;
 
 public class Chica_Behaviour : MonoBehaviour
 {
@@ -10,44 +11,80 @@ public class Chica_Behaviour : MonoBehaviour
     public float timeForClipChange;
     public float timeForKill;
     public VideoPlayer tv;
+
     Animator animator; 
-    public List<VideoClip> videoClipList;
+    public VideoClip[] videoClipList;
+    public VideoClip[] AddsClipList;
+    public AudioSource Growl;
     public AudioSource chicaScreamer;
 
     public Light eye_R;
     public Light eye_L;
+
+    public float minWaitTimeForAdd = 0f;
+    public float maxWaitTimeForAdd = 80f;
+    public float timeToNextAd = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
         angry = false;
         time = 0.0f;
+        timeToNextAd = Random.Range(minWaitTimeForAdd, maxWaitTimeForAdd);
+
+        tv.clip = videoClipList[Random.Range(0, videoClipList.Length)];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(tv.clip == videoClipList[videoClipList.Count-1]) //Last clip will be allways the one that kills the player
+        if (Input.GetKeyDown(KeyCode.V))
         {
-            angry = true;
-        }
-        else 
-        {
-            angry = false;
-            time += Time.deltaTime;
-            if (timeForClipChange < time) 
+            angry = !angry;
+            time = 0;
+            timeToNextAd = Random.Range(minWaitTimeForAdd, maxWaitTimeForAdd);
+
+            if (!angry)
             {
-                tv.clip = videoClipList[videoClipList.Count-1];
-                time = 0.0f;
+                tv.clip = videoClipList[Random.Range(0, videoClipList.Length)];
+            }
+            else
+            {
+                tv.clip = AddsClipList[Random.Range(0, AddsClipList.Length)];
             }
         }
+
+        time += Time.deltaTime;
+
+        if(time >= timeToNextAd && !angry)
+        {
+            tv.clip = AddsClipList[Random.Range(0, AddsClipList.Length)];
+            Growl.Play();
+
+            time = 0;
+            angry = true;
+        }
+
+        //if(tv.clip == videoClipList[videoClipList.Count-1]) //Last clip will be allways the one that kills the player
+        //{
+        //    angry = true;
+        //}
+        //else 
+        //{
+        //    angry = false;
+        //    time += Time.deltaTime;
+        //    if (timeForClipChange < time) 
+        //    {
+        //        tv.clip = videoClipList[videoClipList.Count-1];
+        //        time = 0.0f;
+        //    }
+        //}
 
         if (angry) 
         {
             eye_R.color = Color.red;
             eye_L.color = Color.red;
 
-            time += Time.deltaTime;
             if (time > timeForKill) 
             {
                 Kill();
@@ -64,7 +101,9 @@ public class Chica_Behaviour : MonoBehaviour
     {
         if (angry) 
         {
-            tv.clip = videoClipList[Random.Range(0, videoClipList.Count - 2)]; //Change to any clip that isn't the last one
+            tv.clip = videoClipList[Random.Range(0, videoClipList.Length)];
+            timeToNextAd = Random.Range(minWaitTimeForAdd, maxWaitTimeForAdd);
+            time = 0;
         }
         else 
         {
